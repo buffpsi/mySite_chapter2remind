ae058422d0a80ff7fceede04e63ad68c08e25288
buffpsi <rlawlstjd778@naver.com>
2023-03-21 13:31:08 +0900

    q&a_Entity_set

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne//답변은 하나의 질문에 여러개 달릴 수 있으니 답변(many) 질문(one)
+    private Question question;
+}
