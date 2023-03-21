d5a4f5c974fb34b001627e32874e3c2ef1e9be84
buffpsi <rlawlstjd778@naver.com>
2023-03-21 14:14:53 +0900

    package 분류

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -2,8 +2,12 @@ package com.mysite.sbb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+/*@EnableJpaAuditing는 스프링 데이터 JPA에서 제공하는 기능 중 하나로,
+JPA Entity의 생성일자와 수정일자를 자동으로 관리하기 위한 기능을 활성화하는 어노테이션입니다.*/
 public class SbbApplication {
 
 	public static void main(String[] args) {
