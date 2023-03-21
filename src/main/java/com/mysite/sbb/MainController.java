aebd4398bc9df421b680e95e450c933dfcb4e6a4
buffpsi <rlawlstjd778@naver.com>
2023-03-21 15:36:39 +0900

    sevice 도입

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+
     @GetMapping("/hello")
     @ResponseBody
-    public String HelloOutPut(){
+    public String HelloOutPut() {
         return "hello! 여기 출렸했어요!";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/quenstion/list";
+
     }
 }
---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb.question;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @GetMapping("/question/list")
+    public String lsit(Model model) {
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+}
