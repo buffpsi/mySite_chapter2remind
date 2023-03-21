b7a1c9b83798fe24700c19b799bc8758408f6877
buffpsi <rlawlstjd778@naver.com>
2023-03-21 11:08:03 +0900

    chapter2_start

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,14 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String HelloOutPut(){
+        return "hello! 여기 출렸했어요!";
+    }
+}
