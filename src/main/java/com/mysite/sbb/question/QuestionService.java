fbb2c1811986717c527a65245751eed8fd332da2
buffpsi <rlawlstjd778@naver.com>
2023-03-21 16:46:20 +0900

    bootstrap_desgin

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -2,8 +2,10 @@ package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.mysite.sbb.DataNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +13,16 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        //Optional 클래스의 객체는 해당 값이 존재하는 경우, 값을 가지고 있으며 이를 .get() 메소드를 사용하여 가져올 수 있습니다. 그러나 값이 존재하지 않는 경우, 빈 Optional 객체를 반환합니다.
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+
     }
 
 }
