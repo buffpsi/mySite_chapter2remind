e3473445e7cd432ded6cd8151e213a0e3779bf32
buffpsi <rlawlstjd778@naver.com>
2023-03-28 15:59:34 +0900

    검색기능까지 마무리

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -31,7 +31,7 @@ public class Question {
 //    질문 하나에는 여러개의 답변이 작성될 수 있다.
 //    이때 질문을 삭제하면 그에 달린 답변들도 모두 함께 삭제하기 위해서 @OneToMany의 속성으로 cascade = CascadeType.REMOVE를 사용했다.
     private List<Answer> answerList;
-    @ManyToOne
+    @ManyToOne//question은 많고 쓴사람은 한명이니가
     private SiteUser author;
     @ManyToMany
     Set<SiteUser> voter;
--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -27,13 +27,14 @@ public class QuestionController {
     private final UserService userService;
 
     @GetMapping("/list")
-    public String lsit(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
-        //http://localhost:8080/question/list?page=0 처럼 GET 방식으로 요청된 URL에서 page값을 가져오기 위해
-        //@RequestParam(value="page", defaultValue="0") int page 매개변수가 list 메서드에 추가되었다. URL에 페이지 파라미터 page가 전달되지 않은 경우 디폴트 값으로 0이 되도록 설정했다.
-        Page<Question> paging = this.questionService.getList(page);
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+                       @RequestParam(value = "kw", defaultValue = "") String kw) {
+        Page<Question> paging = this.questionService.getList(page, kw);
         model.addAttribute("paging", paging);
+        model.addAttribute("kw", kw);
         return "question_list";
     }
+
 
     @GetMapping("/detail/{id}") //뒤에 id를 는 밑에 메소드에서 PathVariable로 받는다.
     public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
